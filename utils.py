3c718a6e40d8f10c636178f6804bb455f710e9f9
efti-dot <eftih31@gmail.com>
2026-03-04 03:53:43 +0600

    initial pdf processing implement

--- utils.py
+++ utils.py
@@ -1,7 +1,17 @@
 import os
+import fitz
 from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
 
 def get_openai_client():
     return OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+def extract_text_from_pdf(file_bytes: bytes) -> str:
+    """Extract full text from uploaded PDF"""
+    text = ""
+    with fitz.open(stream=file_bytes, filetype="pdf") as doc:
+        for page in doc:
+            text += page.get_text()
+    return text
