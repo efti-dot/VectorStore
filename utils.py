53d5dcc84c9959677b96f95db335e8a8eec67983
efti-dot <eftih31@gmail.com>
2026-03-04 06:27:59 +0600

    implement processing docx

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
 import fitz
+import io
+from docx import Document
 from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
@@ -14,4 +16,9 @@ def extract_text_from_pdf(file_bytes: bytes) -> str:
     with fitz.open(stream=file_bytes, filetype="pdf") as doc:
         for page in doc:
             text += page.get_text()
+    return text
+
+def extract_text_from_docx(file_bytes: bytes) -> str:
+    doc = Document(io.BytesIO(file_bytes))
+    text = "\n".join([p.text for p in doc.paragraphs if p.text.strip()])
     return text
