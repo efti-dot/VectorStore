6abea2d02d3903b2447ea682f3cfd31033324e5c
efti-dot <eftih31@gmail.com>
2026-03-08 04:19:52 +0600

    img extraction

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import io
 from docx import Document
 from openai import OpenAI
 from dotenv import load_dotenv
+import base64
 load_dotenv()
 
 def get_openai_client():
@@ -22,3 +23,27 @@ def extract_text_from_docx(file_bytes: bytes) -> str:
     doc = Document(io.BytesIO(file_bytes))
     text = "\n".join([p.text for p in doc.paragraphs if p.text.strip()])
     return text
+
+def extract_text_from_image(file_bytes: bytes) -> str:
+        client = get_openai_client()
+        base64_image = base64.b64encode(file_bytes).decode("utf-8")
+
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": "Extract all readable text from this image carefully and focus on the accuracy."},
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}"
+                            }
+                        }
+                    ]
+                }
+            ]
+        )
+
+        return response.choices[0].message.content.strip()
