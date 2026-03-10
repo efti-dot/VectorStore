cc8942bb69b0a93fc11ff03dcfb307cdf1f7093c
efti-dot <eftih31@gmail.com>
2026-03-11 03:04:55 +0600

    creating index and path for metadata

--- vectordb.py
+++ vectordb.py
@@ -1,3 +1,16 @@
 import faiss
 import numpy as np
+import os
 
+class VectorStore:
+    def __init__(self, dim: int, index_path="vector.index", meta_path="metadata.pkl"):
+        self.dim = dim
+        self.index_path = index_path
+        self.meta_path = meta_path
+        self.index = faiss.IndexFlatL2(dim)
+        self.metadata = []
+
+        if os.path.exists(index_path) and os.path.exists(meta_path):
+            self.load()
+
+    
